6bad2d7e8efdb3641d579189adfe7a2b9fb9319f
Yauheni Tsiarokhin <yauheni@tsiarokhin.me>
2021-12-11 16:47:19 +0200

    add back callback data

--- src/callbacks/callback_next.py
+++ src/callbacks/callback_next.py
@@ -3,21 +3,58 @@ import logging
 import tg
 
 
-def handle(token, chat, db_conn, section):
+def handle(token, chat, message, db_conn, section):
     logging.info("Processing /next: (chat: {}, section: {})".format(chat, section))
+
+    prev_section = get_prev_section(section)
 
     data = db_conn.get('{}:data'.format(section))
     if data:
         data = data.decode('utf-8')
-        tg.send_message(token=token, chat=chat, text=data)
+        keyboard = make_data_keyboard(section, prev_section)
+        tg.edit_message(token=token, chat=chat, message=message, text=data, keyboard=keyboard)
     else:
         next_sections = db_conn.smembers(section)
-        keyboard = make_keyboard(section, next_sections)
-        print(keyboard)
-        tg.send_message(token=token, chat=chat, text=section, keyboard=keyboard)
+        keyboard = make_keyboard(section, prev_section, next_sections)
+        tg.edit_message(token=token, chat=chat, message=message, text=section, keyboard=keyboard)
 
 
-def make_keyboard(current_section, next_sections):
+def get_prev_section(section):
+    segments = section.split("/")
+    if len(segments) == 1:
+        return "/root"
+    else:
+        return "/".join(segments[:-1])
+
+
+def make_data_keyboard(current_section, prev_section):
+    keyboard = []
+
+    action_buttons = [
+        {
+            'text': '\U00002b05',
+            'callback_data': '/back@{}'.format(prev_section)
+        },
+        {
+            'text': '\U0001f514',
+            'callback_data': '/subscribe@{}'.format(current_section)
+        },
+        {
+            'text': '\U0001f515',
+            'callback_data': '/unsubscribe@{}'.format(current_section)
+        },
+        {
+            'text': '\U0001f4e3',
+            'callback_data': '/proposal@{}'.format(current_section)
+        },
+    ]
+
+    keyboard.append(action_buttons)
+
+    return keyboard
+
+
+def make_keyboard(current_section, prev_section, next_sections):
     keyboard = []
 
     for next_section in next_sections:
@@ -32,7 +69,7 @@ def make_keyboard(current_section, next_sections):
     action_buttons = [
         {
             'text': '\U00002b05',
-            'callback_data': '/back@{}'.format(current_section)
+            'callback_data': '/back@{}'.format(prev_section)
         },
         {
             'text': '\U0001f514',
--- src/callbacks/common.py
+++ src/callbacks/common.py
@@ -3,4 +3,4 @@ from . import callback_next
 def handle_callback(command, chat, message, db_conn, token):
     if command.startswith("/next"):
         section = command.removeprefix("/next@")
-        callback_next.handle(section=section, chat=chat, db_conn=db_conn, token=token)
+        callback_next.handle(section=section, chat=chat, message=message, db_conn=db_conn, token=token)
